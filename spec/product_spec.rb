42fed67b518690f30e4d0f4ae2fb605afdbe4915
Sean Park <stpnpark@gmail.com>
2017-05-15 21:12:31 +0000

    product rspec test

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,32 +1,61 @@
 require 'rails_helper'
 
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
 
-describe Product, type: :model do
+    it 'should be valid with attributes' do
+      @category = Category.create(name:"Apparel")
+      product = Product.new(
+        name: 'Pants',
+        price: 70,
+        quantity: 4,
+        category_id: @category.id
+        )
+      expect(product).to be_valid
+    end
 
-  it 'is valid with attributes' do
-    product = Category.new.products.new(name: 'sean', description: '123 rails', category_id: 1, quantity: 1, price: 100)
-    expect(product).to be_valid
+    it 'should not be valid without name' do
+      @category = Category.create(name: "Apparel")
+      product = Product.new(
+        name: nil,
+        price: 70,
+        quantity: 4,
+        category_id: @category.id
+        )
+      expect(product).to_not be_valid
+    end
+
+    it 'should not be valid without price' do
+      @category = Category.create(name: "Apparel")
+      product = Product.new(
+        name: 'Pants',
+        price: nil,
+        quantity: 4,
+        category_id: @category.id
+        )
+      expect(product).to_not be_valid
+    end
+
+    it 'should not be valid without quantity' do
+      @category = Category.create(name: "Apparel")
+      product = Product.new(
+        name: 'Pants',
+        price: 70,
+        quantity: nil,
+        category_id: @category.id
+        )
+      expect(product).to_not be_valid
+    end
+
+    it 'should not be valid without category id' do
+      @category = Category.create(name: "Apparel")
+      product = Product.new(
+        name: 'Pants',
+        price: 70,
+        quantity: 4,
+        category_id: nil
+        )
+      expect(product).to_not be_valid
+    end
   end
-  it 'is not valid without a name' do
-    product = Product.new(name: nil)
-    expect(product).to_not be_valid
-  end
-  it 'is not valid without a description' do
-    product = Product.new(description: nil)
-    expect(product).to_not be_valid
-  end
-  it 'is not valid without a category_id' do
-    product = Product.new(category_id: nil)
-    expect(product).to_not be_valid
-  end
-  it 'is not valid without a quantity' do
-    product = Product.new(quantity: nil)
-    expect(product).to_not be_valid
-  end
-  
-  it 'is not valid without a price' do
-    product = Product.new(price: nil)
-    expect(product).to_not be_valid
-  end
-  
 end
